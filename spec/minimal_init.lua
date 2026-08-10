42f0d68dbf3e7ed41f30f0985cbe37f3271da2dd
Mikalai Seva <mikalai.seva@gmail.com>
2026-08-11 00:22:17 +0200

    Simplify spec/minimal_init.lua
    mega.cmdparse/mega.logging now resolve via .busted's lpath (rendered
    by flake.nix), so the manual rtp/package.path splicing loop is no
    longer needed here.

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -1,27 +1,5 @@
---- Run the is file before you run unittests to download any extra dependencies.
-
-local _PLUGINS = {
-    ["https://github.com/ColinKennedy/mega.cmdparse"] = os.getenv("MEGA_CMDPARSE_DIR") or "/tmp/mega.cmdparse",
-    ["https://github.com/ColinKennedy/mega.logging"] = os.getenv("MEGA_LOGGING_DIR") or "/tmp/mega.logging",
-}
-
-local cloned = false
-
-for url, directory in pairs(_PLUGINS) do
-    if vim.fn.isdirectory(directory) ~= 1 then
-        print(string.format('Cloning "%s" plug-in to "%s" path.', url, directory))
-
-        vim.fn.system({ "git", "clone", url, directory })
-
-        cloned = true
-    end
-
-    vim.opt.rtp:append(directory)
-end
-
-if cloned then
-    print("Finished cloning.")
-end
+--- Helper that runs before unittests. `mega.cmdparse`/`mega.logging` come from
+--- `.busted`'s `lpath` (rendered by `flake.nix`, see `bustedConfig`).
 
 vim.opt.rtp:append(".")
 
